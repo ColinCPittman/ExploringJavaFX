0612e9738c0c1884d4a90fb2be0036bf7cae8738
ColinCPittman <Colincpittman@gmail.com>
2023-11-21 12:51:40 -0500

    implemented simple file opening and saving for .txt files only
    Signed-off-by: ColinCPittman <Colincpittman@gmail.com>

--- src/main/java/com/example/exploringjavafx/TextEditorController.java
+++ src/main/java/com/example/exploringjavafx/TextEditorController.java
@@ -6,11 +6,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
 
 public class TextEditorController {
+    File selectedFile;
+    @FXML
+    protected TextArea textArea;
     @FXML
     public void onExitPress(ActionEvent event) {
         try {
@@ -22,6 +30,41 @@ public class TextEditorController {
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void onOpenFilePress(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files (*.txt)", "*.txt"));
+
+        selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            try {
+                String contents = fetchContents(selectedFile);
+                textArea.setText(contents);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String fetchContents(File file) throws IOException {
+        List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        return String.join("\n", lines);
+    }
+
+    public void onSaveFilePress(ActionEvent actionEvent) {
+        if(selectedFile != null) {
+            try{
+                FileWriter writer = new FileWriter(selectedFile);
+                writer.write(textArea.getText());
+                writer.close();
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
